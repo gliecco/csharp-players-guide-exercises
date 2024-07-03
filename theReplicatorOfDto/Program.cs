9bef8c6dd4e8cbf4d4f3b4c87a23103da40b8c47
Juan Pablo G. Lagos <vapour4300@gmail.com>
2024-07-02 21:25:32 -0300

    update directory

--- theReplicatorOfDto/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int[,] matrix = new int[4,4];
-for (int row = 0; row < matrix.GetLength(0); row++)
-{
-for (int column = 0; column < matrix.GetLength(1); column++)
-Console.Write(matrix[row, column] + " ");
-}
